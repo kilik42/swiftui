5d06e767273a495f17a971e92f94b5b2b0a7880d
marvin evins <marvinevins@yahoo.com>
2020-11-14 07:04:32 -0600

    working on observables

--- GridSearch/ContentView.swift
+++ GridSearch/ContentView.swift
@@ -7,10 +7,49 @@
 
 import SwiftUI
 
+class GridViewModel: ObservableObject {
+    @Published var items = 0..<5
+    
+    init(){
+        Timer.scheduledTimer(withTimeInterval: 2, repeats: false) { (_) in
+            self.items = 0..<15 
+        }
+    }
+}
+
 struct ContentView: View {
+    
+    @ObservedObject var vm = GridViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            ScrollView{
+                
+                LazyVGrid(columns: [
+                    GridItem(.flexible(minimum: 100, maximum: 200), spacing: 12),
+                    
+                    GridItem(.flexible(minimum: 100, maximum: 200), spacing: 12),
+                    GridItem(.flexible(minimum: 100, maximum: 200))
+                    
+                ], spacing: 16, content: /*@START_MENU_TOKEN@*/{
+                    ForEach(vm.items, id:\.self){
+                        num in
+                    
+                        VStack(alignment:.leading){
+                            Spacer()
+                                .frame(width: 100, height: 100, alignment:.center).background(Color.blue)
+                        Text("App Title").font(.system(size: 10, weight: .semibold))
+                        Text("App Title").font(.system(size: 9, weight: .regular))
+                        Text("Release Date").font(.system(size: 9, weight: .regular))
+                    }
+//                    .padding()
+                    .background(Color.red)
+                    
+                    }
+                }).padding(.horizontal, 12)
+            }.navigationTitle("GRID SEARCH")
+        }
+       
     }
 }
 
