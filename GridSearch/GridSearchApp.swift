8b53b6e962a4d7c14e5d7e1cf63bfdf6913a1110
marvin evins <marvinevins@yahoo.com>
2020-11-13 19:13:33 -0600

    Initial Commit

---
+++ GridSearch/GridSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridSearchApp.swift
+//  GridSearch
+//
+//  Created by marvin evins on 11/13/20.
+//
+
+import SwiftUI
+
+@main
+struct GridSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
